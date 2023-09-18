bd72f9fd0f79f02e5cc206499a6eaf36da3a6f43
Kyle Washington <kmw1993@msn.com>
2023-09-18 15:10:38 -0400

    Adding Review and Favorite models with their required fields

---
+++ FullStackAuth_WebAPI/Models/Favorites.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Favorites
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+
+        public string BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ThumbnailUrl { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string User { get; set; }
+
+    }
+}
---
+++ FullStackAuth_WebAPI/Models/Reviews.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public double Rating { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string User { get; set; }
+
+      
+    }
+}
