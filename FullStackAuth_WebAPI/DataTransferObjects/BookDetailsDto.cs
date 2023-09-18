f00b9d8e000c6b3126ff0aa7fabef3eea9feaba9
Kyle Washington <kmw1993@msn.com>
2023-09-18 15:23:42 -0400

    Created the BookDetailDto and ReviewWithUSerDto classes and added their fields

---
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class BookDetailsDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string BookId { get; set; }
+
+        public string Url { get; set; }
+    }
+}
